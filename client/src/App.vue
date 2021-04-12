ca647dd7e29465fbd4ab985a90a9bd941d45838e
haeunbae <baehaeun96@gmail.com>
2021-04-12 14:23:07 +0900

    commit

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">HashPhotos</router-link> |
+      <router-link to="/signUp">signUp</router-link>
+    </div>
+    <router-view />
+    <footer>Copyright@ haeun bae</footer>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
